a8fb6263fe25577640a2c6bdcf05eaca4f50082c
Jeremy <senn.jeremy@gmail.com>
2019-04-23 07:53:05 -0700

    Don't confirm join by text users

--- app/workers/create_user_customer_event_account_wallet_worker.rb
+++ app/workers/create_user_customer_event_account_wallet_worker.rb
@@ -4,14 +4,15 @@ class CreateUserCustomerEventAccountWalletWorker
   def perform(event_id, phone)
     event = Event.find(event_id)
     customer = Customer.create(PhoneMobile: phone, CompanyNumber: 7, LangID: 1, Active: 1, GroupID: 5)
-    user = User.create(phone: phone, company_id: 7, role: "basic", customer_id: customer.id, confirmed_at: Time.now)
+#    user = User.create(phone: phone, company_id: 7, role: "basic", customer_id: customer.id, confirmed_at: Time.now)
+    user = User.create(phone: phone, company_id: 7, role: "basic", customer_id: customer.id)
     user.set_temporary_password
     user.save
     account = user.create_event_account(event)
     twilio_client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
     if account
-      body_1 = "Your temporary password is: #{user.temporary_password}"
-#      body_2 = (account.can_fund_by_cc? or account.can_fund_by_ach?) ? " - you can fund your Wallet here: #{edit_account_url(account)}" : " - you can sign in here: #{new_user_session_url}"
+#      body_1 = "Your temporary password is: #{user.temporary_password}"
+      body_1 = ''
       body_2 = (account.can_fund_by_cc? or account.can_fund_by_ach?) ? " - you can fund your Wallet here: https://#{ENV['APPLICATION_HOST']}/accounts/#{account.id}/edit" : " - you can sign in here: https://#{ENV['APPLICATION_HOST']}/users/sign_in"
       message_body = body_1 + body_2
 #      message_media = qr_code_customer_path(user.customer.barcode_access_string)
