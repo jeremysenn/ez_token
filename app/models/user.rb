e2293f98411fb7ac353236184cc2353395f96b87
Jeremy <senn.jeremy@gmail.com>
2019-04-24 08:38:58 -0700

    Remove validatable from User model so can have multiple empty email addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :registerable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable #, :timeoutable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :confirmable #, :validatable, :timeoutable
   
 #  ROLES = %w[admin caddy_admin event_admin basic caddy member consumer payee vendor].freeze
   ROLES = %w[admin basic collaborator].freeze
@@ -31,6 +31,7 @@ class User < ApplicationRecord
       
   validates :phone, uniqueness: true, presence: true 
   validates :email, uniqueness: {allow_blank: true}
+
     
   #############################
   #     Instance Methods      #
