0d730bc2b24454e7d99590cd844eaa553400b416
Jeremy <senn.jeremy@gmail.com>
2019-04-29 14:54:09 -0700

    Forgot password edit page

--- app/models/user.rb
+++ app/models/user.rb
@@ -122,7 +122,7 @@ class User < ApplicationRecord
     unless phone.blank?
       token = self.set_reset_password_token
       reset_password_link = "#{Rails.application.routes.default_url_options[:host]}/users/password/edit?reset_password_token=#{token}"
-      message = "Someone has requested a link to change your password. You can do this through the link below. If you didn't request this, please ignore this message. Your password won't change until you access the link below and create a new one. #{reset_password_link}"
+      message = "Someone has requested a link to change your password. You can do this through the link below. Your password won't change until you access the link below and create a new one. #{reset_password_link}"
       client = Savon.client(wsdl: "#{ENV['EZCASH_WSDL_URL']}")
       client.call(:send_sms, message: { Phone: phone, Msg: "#{message}"})
     end
