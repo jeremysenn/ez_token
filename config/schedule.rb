83719125653910e1fe198586654fafe102e39cdb
Jeremy <senn.jeremy@gmail.com>
2019-05-06 09:28:53 -0700

    Auto restart with schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -28,6 +28,8 @@ every :reboot do
   # Mac Mini
 #  job_type :application, "cd /Users/jeremy/Ruby/ez_token && :task :output"
   
-  application "rails server -p 3003" # Start application server
+#  application "rails server -p 3003" # Start application server
+  application "rails server -b 0.0.0.0 -p 3003"
+
   application "bundle exec sidekiq -c 5" # Start background workers
 end
