b6b3b7726ff41d8df237c7ad451e7fe6f82c9200
Jeremy <senn.jeremy@gmail.com>
2019-05-10 06:42:09 -0700

    Timeout user after 10 minutes of inactivity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :registerable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable #, :timeoutable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable, :timeoutable
   
 #  ROLES = %w[admin caddy_admin event_admin basic caddy member consumer payee vendor].freeze
   ROLES = %w[admin basic collaborator].freeze
