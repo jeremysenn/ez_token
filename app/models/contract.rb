59cf462936d12556ba0b3f22b64d5c4e63a20655
Jeremy <senn.jeremy@gmail.com>
2019-07-18 06:50:38 -0700

    Publish, unpublish, and new_version for contracts

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -4,19 +4,64 @@ class Contract < ActiveRecord::Base
   
   belongs_to :company
   has_many :account_types
-  has_many :signatures
+#  has_many :signatures
   
+#  has_many :same_name, class_name: 'Contract',
+#             primary_key: :name, foreign_key: :name
+#  
+#  def is_published?
+#    !version.nil?
+#  end
+  
+  #########################################
+  has_many :signatures, dependent: :destroy, inverse_of: :contract
+
   has_many :same_name, class_name: 'Contract',
-             primary_key: :name, foreign_key: :name
-  
-  #############################
-  #     Instance Methods      #
-  #############################
-  
+           primary_key: :name, foreign_key: :name
+
+  before_validation :downcase_name
+  before_update :no_signatures
+  before_destroy :no_signatures
+
+  validates :name, presence: true, format: /\A[\w-]+\z/
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :version, uniqueness: { scope: :name, case_sensitive: false },
+                      allow_nil: true
+
+  default_scope -> { order(:name, version: :desc) }
+
+  scope :published, -> { where.not(version: nil) }
+  scope :latest, -> do
+    fpc = arel_table
+    sn = Arel::Table.new(:same_names_fine_print_contracts)
+    joins(:same_name).group(:id).having(fpc[:version].eq(sn[:version].maximum))
+  end
+  scope :signed_by, ->(user) do
+    joins(:signatures).where(
+      # Originally this would be:
+      # signatures: { user_id: user.id, user_type: class_name_for(user) }
+      # This is broken in Rails 5 without a monkeypatch, so instead we use the following for now:
+      fine_print_signatures: { user_id: user.id, user_type: class_name_for(user) }
+    )
+  end
+
   def is_published?
     !version.nil?
   end
-  
+
+  def is_latest?
+    is_published? && self == same_name.published.first
+  end
+
+  def signed_by?(user)
+    user_class_name = self.class.class_name_for(user)
+    signatures.loaded? ?
+      signatures.any? do |signature|
+        signature.user_id == user.id and signature.user_type == user_class_name
+      end : signatures.where(user_id: user.id, user_type: user_class_name).exists?
+  end
+
   def publish
     errors.add(:base, I18n.t('fine_print.contract.errors.already_published')) \
       if is_published?
@@ -42,10 +87,26 @@ class Contract < ActiveRecord::Base
     Contract.where(name: name, version: nil).first || \
       dup.tap{|contract| contract.version = nil}
   end
-  
-  #############################
-  #     Class Methods         #
-  #############################
-  
+
+  ##############
+  # Validation #
+  ##############
+
+  def no_signatures
+    return if signatures.empty?
+    errors.add(:base, I18n.t('fine_print.contract.errors.already_signed'))
+    throw :abort
+  end
+
+  protected
+
+  def downcase_name
+    self.name = name.try(:downcase)
+  end
+
+  def self.class_name_for(user)
+    user.class.base_class.name
+  end
+
   
 end
