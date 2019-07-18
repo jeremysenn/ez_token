39dd823557044ea772665cc6cd89f462ccfd29db
Jeremy <senn.jeremy@gmail.com>
2019-07-18 06:47:31 -0700

    Publish, unpublish, and new_version for contracts

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -17,6 +17,32 @@ class Contract < ActiveRecord::Base
     !version.nil?
   end
   
+  def publish
+    errors.add(:base, I18n.t('fine_print.contract.errors.already_published')) \
+      if is_published?
+    return false unless errors.empty?
+
+    self.version = (same_name.published.first.try(:version) || 0) + 1
+
+    save.tap do |success|
+      instance_exec(self, &FinePrint.config.contract_published_proc) if success
+    end
+  end
+
+  def unpublish
+    errors.add(:base, I18n.t('fine_print.contract.errors.not_latest')) \
+      unless is_latest?
+    return false unless errors.empty?
+
+    self.version = nil
+    save
+  end
+
+  def new_version
+    Contract.where(name: name, version: nil).first || \
+      dup.tap{|contract| contract.version = nil}
+  end
+  
   #############################
   #     Class Methods         #
   #############################
