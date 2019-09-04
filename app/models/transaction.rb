595aad51e378e7fe4193e803fa256a831e0c00d8
Jeremy <senn.jeremy@gmail.com>
2019-09-04 10:31:02 -0700

    Use to_acct_id and from_acct_id instead of nbr for both in transactions csv

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -488,7 +488,7 @@ class Transaction < ActiveRecord::Base
   
   def self.export_to_csv(transactions)
     require 'csv'
-    attributes = %w{tranID date_time Description dev_id error_code tran_code sec_tran_code from_acct_nbr to_acct_nbr amt_req amt_auth ChpFee}
+    attributes = %w{tranID date_time Description dev_id error_code tran_code sec_tran_code from_acct_id to_acct_id amt_req amt_auth ChpFee}
     
     CSV.generate(headers: true) do |csv|
       csv << attributes
