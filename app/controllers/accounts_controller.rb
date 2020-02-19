bf475fcc816e1a45122bf756d1ca0f58e9f323c1
Jeremy <senn.jeremy@gmail.com>
2020-02-19 08:55:02 -0800

    Change name of payment amount field so doesn't conflict with request payment amount on same page for collaborators

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -98,7 +98,7 @@ class AccountsController < ApplicationController
   end
   
   def one_time_payment
-    amount = params[:payment_amount].to_f.abs unless params[:amount].blank?
+    amount = params[:payment_amount].to_f.abs unless params[:payment_amount].blank?
     note = params[:note]
     receipt_number = params[:receipt_number]
     to_customer_id = params[:to_customer_id]
