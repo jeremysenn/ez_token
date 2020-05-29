7f90b8c378f45c93449821d63a1f42a8f403ea6e
Jeremy <senn.jeremy@gmail.com>
2020-05-29 10:22:03 -0700

    Use gmail username in application_mailer by default, to test why not sending

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'info@tranact.com'
+#  default from: 'info@tranact.com'
+  default from: "#{ENV['GMAIL_USERNAME']}"
   layout 'mailer'
   
   def send_admins_transaction_dispute_email_notification(user, to_emails, transaction, details)
